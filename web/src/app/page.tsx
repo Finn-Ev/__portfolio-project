deb4af3faa6e6dcecfafd3d47f45607c77f448dc
Finn-Ev <finn.evermann@icloud.com>
2024-01-28 18:59:07 +0100

    [WEB] setup avenir font

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div className="text-3xl">Home</div>;
+  return (
+    <>
+      <div className="text-3xl font-bold">Lorem Ipsum</div>
+      <div className="text-3xl font-medium">Lorem Ipsum</div>
+      <div className="text-3xl font-normal">Lorem Ipsum</div>
+    </>
+  );
 }
