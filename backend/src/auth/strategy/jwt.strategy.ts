0145fe0744bee2816228f9cc1cef2dd4cb1ea5a8
Finn-Ev <finn.evermann@icloud.com>
2024-02-08 00:35:11 +0100

    [BACKEND]  jwt-strategy: clean up cookie-leftovers

--- backend/src/auth/strategy/jwt.strategy.ts
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -13,23 +13,9 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     private prisma: PrismaService,
   ) {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        // first we try to extract the token from the cookies
-        // this is handy for web-clients
-        JwtStrategy.extractFromCookies,
-        // if it's not there we try to extract it from the header
-        // this is handy for non-web clients like mobile apps
-        ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ]),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
-  }
-
-  private static extractFromCookies(req: RequestType): string | null {
-    if (req.cookies && req.cookies.user_token && req.cookies.user_token.access_token) {
-      return req.cookies.user_token.access_token;
-    }
-    return null;
   }
 
   async validate(payload: JwtPayload) {
@@ -38,6 +24,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
         id: payload.userId,
       },
     });
+
     delete user.pwHash;
     return user; // this will be added to the request object when the value is truthy
   }
