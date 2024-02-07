898c63f7300afcfadb17c5f81acfe7695b083edc
Finn-Ev <finn.evermann@icloud.com>
2024-02-08 00:26:18 +0100

    [WEB] setup basic bookmark-list page

--- web/src/app/bookmarks/page.tsx
+++ web/src/app/bookmarks/page.tsx
@@ -1,7 +1,28 @@
-export default function BookmarksPage() {
+import BookmarkForm from '../../components/bookmarkForm';
+import ErrorMessage from '../../components/errorMessage';
+import { getAllBookmarks } from '../../lib/actions/bookmarks/getAll';
+
+export default async function BookmarksPage() {
+  const { value: bookmarks, error } = await getAllBookmarks();
+
+  if (error) {
+    return <ErrorMessage message={error.message} />;
+  }
+
   return (
-    <div>
+    <div className="flex justify-between w-full">
       <h1>Here are all your bookmarks</h1>
+      <BookmarkForm />
+
+      {bookmarks!.length === 0 ? (
+        <div>Create your first bookmark.</div>
+      ) : (
+        <ul>
+          {bookmarks!.map((bookmark) => (
+            <li key={bookmark!.id}>{bookmark.title}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
