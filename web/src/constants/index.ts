54928de73e84d83e5e904815953b59584a632113
Finn-Ev <finn.evermann@icloud.com>
2024-02-08 00:33:04 +0100

    [WEB] adjust contant-values and usage

--- web/src/constants/index.ts
+++ web/src/constants/index.ts
@@ -1,2 +1,2 @@
-export const USER_TOKEN_COOKIE_NAME = 'userToken';
-export const USER_ROOT_CATEGORY_ID_COOKIE_NAME = 'userRootCategoryId';
+export const USER_TOKEN_COOKIE_NAME = 'user_token';
+export const USER_ROOT_CATEGORY_ID_COOKIE_NAME = 'user_root_category_id';
--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,9 +1,10 @@
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { USER_TOKEN_COOKIE_NAME } from './constants';
 
 export async function middleware(request: NextRequest) {
-  const userToken = cookies().get('user_token')?.value;
+  const userToken = cookies().get(USER_TOKEN_COOKIE_NAME)?.value;
 
   if (request.nextUrl.pathname === '/' && !userToken) {
     return NextResponse.redirect(new URL('/auth', request.url));
