d4d4fdc7e86fb5dc25ad18afee2e84891d8ecef4
Finn-Ev <finn.evermann@icloud.com>
2024-02-08 00:52:19 +0100

    [WEB] add bookmarkForm

---
+++ web/src/components/bookmarkForm.tsx
@@ -0,0 +1,129 @@
+'use client';
+
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { PlusCircle } from 'lucide-react';
+import { Button } from './ui/button';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
+import { useToast } from './ui/toast/use-toast';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createBookmark } from '../lib/actions/bookmarks/create';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+// interface BookmarkFormProps {
+//   callback: () => void;
+//   defaultValues: {
+//     title: string;
+//     link: string;
+//     description?: string | undefined;
+//   };
+// }
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: 'Enter a title' }),
+  link: z.string().url({ message: 'Invalid URL' }),
+  description: z.string().optional(),
+});
+
+export default function BookmarkForm() {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+      link: '',
+      description: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const response = await createBookmark(values);
+
+    if (response) {
+      toast({
+        title: 'Bookmark created!',
+      });
+      setOpen(false);
+      router.refresh();
+      form.reset();
+    } else {
+      toast({
+        title: 'Error',
+        description: 'Something went wrong. Please try again.',
+        variant: 'destructive',
+      });
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          Create Bookmark <PlusCircle className="ml-2" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create a bookmark</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-full">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-foreground">Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="link"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-foreground">Link</FormLabel>
+                  <FormControl>
+                    <Input placeholder="URL" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-foreground">Description (optional)</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="mt-3 w-full">
+              Submit.
+            </Button>
+          </form>
+        </Form>
+        {/* <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter> */}
+      </DialogContent>
+    </Dialog>
+  );
+}
