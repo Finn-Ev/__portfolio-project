07312ad83a3e7c395cb120d9575aed8147b9afd6
Finn-Ev <finn.evermann@icloud.com>
2024-02-15 18:32:17 +0100

    [WEB] add update bookmark action

---
+++ web/src/lib/actions/bookmarks/update.ts
@@ -0,0 +1,15 @@
+'use server';
+
+import { fetchBackend } from '../fetchBackend';
+
+export async function updateBookmark(
+  bookmarkId: number,
+  data: {
+    title: string;
+    link: string;
+    categoryId?: number;
+    description?: string;
+  },
+) {
+  return await fetchBackend('PATCH', `/bookmarks/${bookmarkId}`, data);
+}
