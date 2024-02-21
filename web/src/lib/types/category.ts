14d5eb61ff10bd88cc8b0a77a58f54899bc78765
Finn <finn.evermann@icloud.com>
2024-02-21 22:01:43 +0100

    [WEB] adapt to latest category-fetching backend changes

--- web/src/lib/types/category.ts
+++ web/src/lib/types/category.ts
@@ -1,8 +1,11 @@
+import { Bookmark } from './bookmark';
+
 export type Category = {
   id: number;
   title: string;
   description?: string;
   userId: number;
+  bookmarks: Bookmark[];
 
   createdAt: string;
   updatedAt: string;
