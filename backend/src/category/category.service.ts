574e0e46ce63c72ffe80f1f6128b49e0ef014759
Finn <finn.evermann@icloud.com>
2024-02-25 21:58:17 +0100

    [BACKEND] misc

--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { PrismaService } from '../prisma/prisma.service';
