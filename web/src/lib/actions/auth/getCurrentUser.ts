5da0aeb4175d6a156c5bd8ac905f977482a208bd
Finn <finn.evermann@icloud.com>
2024-04-02 18:04:45 +0200

    [WEB] misc

--- web/src/lib/actions/auth/getCurrentUser.ts
+++ web/src/lib/actions/auth/getCurrentUser.ts
@@ -4,15 +4,11 @@ import { User } from '../../types/user';
 import { fetchBackend } from '../fetch-backend';
 
 export async function getCurrentUser(): Promise<User | null> {
-  try {
-    const response = await fetchBackend<User>('GET', '/users/me');
+  const response = await fetchBackend<User>('GET', '/users/me');
 
-    if (response.success) {
-      return response.value!;
-    }
-
-    return null;
-  } catch (error) {
-    throw error; // TODO
+  if (response.success) {
+    return response.value!;
   }
+
+  return null;
 }
