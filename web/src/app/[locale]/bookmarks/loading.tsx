88a15e7b1e4db9418f63afc04d3a5421a877bef1
Finn <finn.evermann@icloud.com>
2024-04-09 19:30:33 +0200

    [WEB] use react-spinners instead of react-loader-spinner

--- web/src/app/[locale]/bookmarks/loading.tsx
+++ web/src/app/[locale]/bookmarks/loading.tsx
@@ -1,19 +1,12 @@
 'use client';
 
-import { Oval } from 'react-loader-spinner';
+import { PulseLoader } from 'react-spinners';
 
 export default function Loading() {
   return (
     // TODO apply theme colors
     <div className="flex h-full items-center justify-center">
-      <Oval
-        height="80"
-        width="80"
-        ariaLabel="oval-loading"
-        color="#FFF"
-        secondaryColor="#F2F2F2"
-        wrapperClass="-mt-5"
-      />
+      <PulseLoader color="#000" />
     </div>
   );
 }
