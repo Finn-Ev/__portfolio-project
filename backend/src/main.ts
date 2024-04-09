302bd100b25f0f6e2c1764282b7b59fc4877b818
Finn <finn.evermann@icloud.com>
2024-04-09 17:14:15 +0200

    [BACKEND] add production-only env logic

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,14 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-export const PORT = 3001;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors();
 
-  await app.listen(PORT);
+  if (process.env.NODE_ENV === 'production') {
+    app.enableCors({
+      origin: process.env.WEB_URL,
+      credentials: true,
+    });
+    await app.listen(process.env.PORT, process.env.HOST);
+  } else {
+    app.enableCors();
+    await app.listen(3001);
+  }
 }
 bootstrap();
